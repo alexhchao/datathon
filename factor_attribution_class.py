fixed several calculation errors in factor Attribution object

factor_attribution_class.py
@@ -8,6 +8,7 @@ class factorAttribution(object):
 
     def __init__(self,
                  V,
+                 F,
                  h,
                  S,
                  R):
@@ -15,7 +16,8 @@ class factorAttribution(object):
         
         Parameters
         ----------
-        V - covariance matrix (n x n)
+        V - stock covariance matrix (n x n)
+        F - factor covariance matrix (k x k)
         h - holdings (n x 1)
         S - source or factor portfolios (n x k), k = num factors
         R - future returns (n x 1)
@@ -25,6 +27,7 @@ class factorAttribution(object):
             raise ValueError(" Cov matrix V is not square!")
 
         self.V = V # n x n
+        self.F = F # k x k
         self.h = h # n x 1
         self.S = S # n x k
         self.R = R # n x 1
@@ -51,10 +54,14 @@ class factorAttribution(object):
         #self.port_factor_exposure = self.h.dot(self.S)
 
         # B) Vol Adj Exposure
-        self.factor_vol = (np.sqrt(np.diag(self.V)))
+        self.factor_vol = (np.sqrt(np.diag(self.F))) # this should be F not V!
 
         # vol_adj_factor_exp = h.T.dot(S)*factor_vol
-        self.vol_adj_factor_exposure = self.port_factor_exposure * self.factor_vol
+        try:
+            self.vol_adj_factor_exposure = self.port_factor_exposure * self.factor_vol
+        except Exception as e:
+            print(e)
+            import pdb; pdb.set_trace()
 
         # -4.57%, 25.5%, 10.54%
 
@@ -64,19 +71,26 @@ class factorAttribution(object):
         # D) Risk Contribution (%)
         self.risk_contrib_from_factors_pct = self.risk_contrib_from_factors / self.port_vol
 
+        # port returns
+        self.port_returns = self.h.T.dot(R)
+
+        self.factor_returns = self.S.T.dot(R)
+
         # E) Return contribution
-        self.return_contrib_from_factors = self.h.T * (self.R)
+        #self.return_contrib_from_factors = self.h.T * (self.R) # this is wrong...
+#        self.return_contrib_from_factors = self.R.T.dot(S)*B.dot(factor_rets )
+        self.return_contrib_from_factors = B*(self.factor_returns)
 
         # resid portfolio
-        self.u = self.h - self.S.T.dot(B)
+        self.u = self.h - self.S.dot(B)
         self.resid_port = self.u
 
         # risk contrib from resid
-        self.risk_contrib_from_resid = self.h.T.dot(self.V)*(self.u)/ self.port_vol
+        self.risk_contrib_from_resid = self.h.T.dot(self.V).dot(self.u)/ self.port_vol
         # should these be split out into factors? no right? (replace * with dot)
 
         # return contrib from resid
-        self.return_contrib_from_resid = self.u.T * (self.R)
+        self.return_contrib_from_resid = self.u.T.dot(self.R)
         # should these be split out into factors? no right? (replace * with dot)
 
     def check_all_dims_align(self):
@@ -88,6 +102,7 @@ class factorAttribution(object):
         =======================
         Portfolio Variance = {}
         Portfolio Vol = {}
+        Portfolio Returns = {}
         
         Exposures
         =========
@@ -108,12 +123,13 @@ class factorAttribution(object):
         
         """.format(self.port_var,
                     self.port_vol,
+                   self.port_returns,
                     self.port_factor_exposure,
                     self.vol_adj_factor_exposure,
                    self.risk_contrib_from_factors,
                    self.risk_contrib_from_factors_pct,
                    self.risk_contrib_from_resid,
-                   self.return_contrib_from_factors,
+                   [x for x in self.return_contrib_from_factors],
                    self.return_contrib_from_resid
                    ))
 

factor_model_revisit.py
@@ -84,7 +84,54 @@ model.calculate_stock_covariance_matrix()
 
 cov_to_corr(model.all_stock_covariance_mat[dt])
 
+############
+# lets run some attributions!
+dt
 
+factor_port = model.factor_portfolios.query("date==@dt").query(" factor=='momentum' ").set_index('stock').weight
+other_ports = model.factor_portfolios.query("date==@dt").query(" factor!='momentum' ").set_index(['stock','factor']).weight.unstack()
+
+factor_this_dt = model.factor_portfolios.query("date==@dt")
+
+factor_this_dt[factor_this_dt.factor.isin(list_factors)].set_index(['stock','factor']).weight.unstack()
+
+list_stocks = factor_port.index
+fwd_rets = model.df.query("date == @dt").reset_index()[model.df.query(
+    "date == @dt").reset_index().stock.isin(list_stocks)].set_index('stock').fwd_returns*0.01
+
+list_factors = other_ports.columns
+
+factor_cov = model.all_factor_covariance_mat[dt]
+
+
+factor_exp = factorAttribution(V = np.array(model.all_stock_covariance_mat[dt]),
+                               F = np.array(factor_cov.loc[list_factors ,list_factors ]),
+                     h = np.array(factor_port) ,
+                     S = np.array(other_ports) ,
+                     R = np.array(fwd_rets))
+factor_exp
+
+[x for x in factor_exp.return_contrib_from_factors]
+
+# factor returns
+factor_rets = factor_exp.S.T.dot(R)
+
+factor_exp.port_factor_exposure.dot(factor_rets )
+
+factor_exp.S.dot(factor_exp.port_factor_exposure)
+
+factor_exp.factor_vol
+
+
+#############
+# save down factor and spec returns!
+
+_factor_portfolios = model.factor_portfolios.copy()
+_factor_portfolio_returns = model.factor_returns.copy()
+
+#model.factor_wealth.iloc[:,:6].plot()
+
+_specific_returns = model.specific_returns.copy()
 
 ##############
 
@@ -139,12 +186,6 @@ get_exp_weights(window = 16,half_life = 8)
 
 [(0.5)**((12-t)/6) for t in range(1,13)]
 
-#############
-# save down factor and spec returns!
-
-_factor_portfolios = model.factor_portfolios.copy()
-_factor_portfolio_returns = model.factor_returns.copy()
-_specific_returns = model.specific_returns.copy()
 
 
 model_2 = factor_risk_model(df = df_new)

risk_attribution.py
@@ -6,6 +6,8 @@ import pandas as pd
 
 V = np.array([[0.0232, 0.0163, 0.0095],[0.0163, 0.2601, 0.0122],[0.0095, 0.0122, 0.0174]])
 
+F = V
+
 h = np.array([-0.3, 0.5, 0.8])
 
 S = np.identity(3)
@@ -60,8 +62,10 @@ factor_attrib.u
 
 factor_attrib = factorAttribution(V=V,
                  h=h,
-                 S=np.identity(4),
+                F = F,
+                 S=np.identity(3),
                  R=R)
+factor_attrib
 
 factor_attrib.port_var
 factor_attrib.port_vol